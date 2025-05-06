c8d643c68e214907dcd56dd416967217ef35dafb
Ykuroyy <yykm666@gmail.com>
2025-05-06 16:13:38 +0900

    初期設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_06_065207) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
