900b415de99dc372e2d271fde0c92312feaa6c61
Ykuroyy <yykm666@gmail.com>
2025-05-12 11:06:04 +0900

    debug確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,12 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order(created_at: :desc)
+
+    return unless Rails.env.production?
+
+    Rails.logger.info "【本番デバッグ】Item.count = #{Item.count}"
+    Rails.logger.info "【本番デバッグ】最新商品名: #{Item.last&.name}"
+    Rails.logger.info "【本番デバッグ】画像添付あり？: #{Item.last&.image&.attached?}"
   end
 
   def show
