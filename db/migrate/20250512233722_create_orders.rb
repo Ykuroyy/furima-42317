b7c3d657098be254b6eae854081ac3be4c62ebe1
Ykuroyy <yykm666@gmail.com>
2025-05-14 12:15:11 +0900

    orderテーブルのdb追記

--- db/migrate/20250512233722_create_orders.rb
+++ db/migrate/20250512233722_create_orders.rb
@@ -1,6 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
-    create_table :orders do |t|
+     create_table :orders, if_not_exists: true do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
       t.integer :price, null: false 
